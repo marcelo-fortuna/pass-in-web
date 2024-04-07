c7c68f6d32ae261abf87c63ac1577562981b6fac
Marcelo Fortuna <celofortuna@gmail.com>
2024-04-06 22:01:02 -0300

    feat: Creating an funcionality for pagination.
    feat: Addition opacity to pagination buttons if disabled.
    
    docs: Addition of faker-js to generate fake data.
    
    docs: Addition of dayjs to deal with dates.

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,11 +1,46 @@
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from "lucide-react"
+import dayjs from "dayjs"
+import 'dayjs/locale/pt-br'
+import relativeTime from "dayjs/plugin/relativeTime"
 import { IconButton } from "./icon-button"
 import { Table } from "./table/table"
 import { TableHeader } from "./table/table-header"
 import { TableCell } from "./table/table-cell"
 import { TableRow } from "./table/table-row"
+import { ChangeEvent, useState } from "react"
+import { attendees } from "../data/attendees"
+
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
 
 export function AttendeeList() {
+    const [search, setSearch] = useState('')
+    const [page, setPage] = useState(1)
+
+    const totalPages = Math.ceil(attendees.length / 10)
+
+    function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+        setSearch(event.target.value)
+    }
+
+    function goToFirstPage() {
+        setPage(1)
+    }
+
+    function goToLastPage() {
+        setPage(totalPages)
+    }
+
+    function goToNextPage() {
+        // if (page < totalPages)
+        setPage(page + 1)
+    }
+
+    function goToPreviousPage() {
+        // if (page > 1)
+        setPage(page - 1)
+    }
+
     return (
         <div className="flex flex-col gap-4">
             <div className="flex gap-3 items-center">
@@ -13,8 +48,9 @@ export function AttendeeList() {
 
                 <div className='px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3'>
                     <Search className="size-4 text-emerald-300" />
-                    <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..."></input>
+                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..."></input>
                 </div>
+                {search}
             </div>
 
             <Table>
@@ -31,21 +67,21 @@ export function AttendeeList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {Array.from({ length: 8 }).map((_, i) => {
+                    {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
                         return (
-                            <TableRow key={i}>
+                            <TableRow key={attendee.id}>
                                 <TableCell>
                                     <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
                                 </TableCell>
-                                <TableCell>12383</TableCell>
+                                <TableCell>{attendee.id}</TableCell>
                                 <TableCell>
                                     <div className="flex flex-col gap-1">
-                                        <span className="font-semibold text-white">Marcelo dos Santos Fortuna</span>
-                                        <span>celofortuna@gmail.com</span>
+                                        <span className="font-semibold text-white">{attendee.name}</span>
+                                        <span>{attendee.email}</span>
                                     </div>
                                 </TableCell>
-                                <TableCell>7 dias atrás</TableCell>
-                                <TableCell>3 dias atrás</TableCell>
+                                <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                                 <TableCell>
                                     {/* <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
                                             <MoreHorizontal className="size-4" />
@@ -61,23 +97,23 @@ export function AttendeeList() {
                 <tfoot>
                     <tr>
                         <TableCell colSpan={3}>
-                            Mostrando 10 de 228 itens
+                            Mostrando 10 de {attendees.length} itens
                         </TableCell>
                         <TableCell className="text-right" colSpan={3}>
                             <div className="inline-flex items-center gap-8">
-                                <span>Página 1 de 23</span>
+                                <span>Página {page} de {totalPages}</span>
 
                                 <div className="flex gap-1.5">
-                                    <IconButton>
+                                    <IconButton onClick={goToFirstPage} disabled={page === 1}>
                                         <ChevronsLeft className="size-4" />
                                     </IconButton>
-                                    <IconButton>
+                                    <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                                         <ChevronLeft className="size-4" />
                                     </IconButton>
-                                    <IconButton>
+                                    <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                                         <ChevronRight className="size-4" />
                                     </IconButton>
-                                    <IconButton>
+                                    <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                                         <ChevronsRight className="size-4" />
                                     </IconButton>
                                 </div>
--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonProps extends ComponentProps<'button'> {
     transparent?: boolean
@@ -8,9 +9,15 @@ export function IconButton({ transparent, ...props }: IconButtonProps) {
     return (
         <button
             {...props}
-            className={transparent
-                ? "bg-black/20 border border-white/10 rounded-md p-1.5"
-                : "bg-white/10 border border-white/10 rounded-md p-1.5"}
+            // className={transparent
+            //     ? "bg-black/20 border border-white/10 rounded-md p-1.5"
+            //     : "bg-white/10 border border-white/10 rounded-md p-1.5"
+            // }
+            className={twMerge(
+                'border border-white/10 rounded-md p-1.5',
+                transparent ? 'bg-black/20' : 'bg-white/10',
+                props.disabled ? 'opacity-50' : null,
+            )}
         />
     )
 }
